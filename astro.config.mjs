fa1ea52032f151cd547e5390bb9d23ddf3918c59
David Dirjan <office@axiontic.com>
2026-03-30 23:35:10 +0300

    Secondary pages + Romanian Translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://axionly.io',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ro'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
